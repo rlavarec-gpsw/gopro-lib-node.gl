d9aa039ef124c20ebd166aaf950dfb3f372830a0
Jeff Moguillansky <jmoguillansky@gopro.com>
2021-03-11 16:45:17 -0800

    build: set environment variables in parent shell, instead of each time a command is invoked
    Signed-off-by: Bill MacDonald <wmacdonald@gopro.com>

---
+++ build_scripts/windows/set_env.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+if [[ ! -v VCPKG_DIR ]]; then
+  export VCPKG_DIR='C:\vcpkg'
+fi
+
+CURRENT_DIR=`wslpath -wa .`
+export PKG_CONFIG=$CURRENT_DIR'\venv\Scripts\pkgconf.exe'
+export TARGET_OS=Windows
+
+export DEBUG_GPU_CAPTURE=yes
+
+export PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1
+export PKG_CONFIG_ALLOW_SYSTEM_LIBS=1
+export PKG_CONFIG_DONT_DEFINE_PREFIX=1
+export PKG_CONFIG_PATH=$VCPKG_DIR'\installed\x64-windows\lib\pkgconfig;'$CURRENT_DIR'\venv\Lib\pkgconfig'
+export WSLENV=PKG_CONFIG/w:PKG_CONFIG_PATH/w:PKG_CONFIG_ALLOW_SYSTEM_LIBS/w:PKG_CONFIG_ALLOW_SYSTEM_CFLAGS/w:PKG_CONFIG_DONT_DEFINE_PREFIX/w:VCVARS64/w
