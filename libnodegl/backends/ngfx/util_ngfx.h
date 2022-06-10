f01b690e015934c2373fbb1cf280b38a143a7c16
Jeff Moguillansky <jmoguillansky@gopro.com>
2022-06-10 10:51:38 -0700

    backends/ngfx: add ngfx backend files

---
+++ libnodegl/backends/ngfx/util_ngfx.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2019 GoPro Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef UTIL_NGFX_H
+#define UTIL_NGFX_H
+
+#include "format_ngfx.h"
+#include "ngfx/graphics/GraphicsContext.h"
+#include "ngfx/graphics/GraphicsCore.h"
+#include "rendertarget.h"
+#include "topology_ngfx.h"
+
+ngfx::FilterMode to_ngfx_filter_mode(int filter);
+ngfx::FilterMode to_ngfx_mip_filter_mode(int filter);
+ngfx::TextureType to_ngfx_texture_type(int type);
+ngfx::IndexFormat to_ngfx_index_format(int indices_format);
+ngfx::BlendFactor to_ngfx_blend_factor(int blend_factor);
+ngfx::BlendOp to_ngfx_blend_op(int blend_op);
+ngfx::CompareOp to_ngfx_compare_op(int compare_op);
+ngfx::StencilOp to_ngfx_stencil_op(int stencil_op);
+ngfx::ColorComponentFlags to_ngfx_color_mask(int color_write_mask);
+ngfx::CullModeFlags to_ngfx_cull_mode(int cull_mode);
+ngfx::ImageUsageFlags to_ngfx_image_usage_flags(int usage_flags);
+ngfx::AttachmentLoadOp to_ngfx_load_op(int op);
+ngfx::AttachmentStoreOp to_ngfx_store_op(int op);
+ngfx::RenderPass *get_render_pass(ngfx::GraphicsContext *ctx,
+                                  const struct rendertarget_params *params);
+ngfx::RenderPass *get_compat_render_pass(ngfx::GraphicsContext *ctx,
+                                         const struct rendertarget_desc &desc);
+
+inline uint32_t get_bpp(int format)
+{
+    return ngli_format_get_bytes_per_pixel(format);
+}
+
+#endif
