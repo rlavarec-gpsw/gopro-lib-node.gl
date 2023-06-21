a5545a9f95cac1534be370c7ddb2fa254e136cd8
fallera-gpsw <134376906+fallera-gpsw@users.noreply.github.com>
2023-06-21 11:24:56 +0200

    build: set env variable REQUESTS_CA_BUNDLE and SSL_CERT_FILE with certifi.where() to fix certificate error

--- configure.py
+++ configure.py
@@ -26,6 +26,7 @@ import hashlib
 import logging
 import os
 import os.path as op
+import certifi
 import pathlib
 import platform
 import shlex
@@ -545,10 +546,8 @@ class _EnvBuilder(venv.EnvBuilder):
 
     def post_setup(self, context):
         if _SYSTEM == "MinGW":
-            pip_install = [context.env_exe, "-m", "pip", "install"]
-            pip_install += ["certifi"]
-            logging.info("install certificate dependencies: %s", _cmd_join(*pip_install))
-            run(pip_install, check=True)
+            os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+            os.environ["SSL_CERT_FILE"] = certifi.where()
             return
         pip_install = [context.env_exe, "-m", "pip", "install"]
         pip_install += ["meson", "ninja"]
