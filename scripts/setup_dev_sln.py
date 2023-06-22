4603e286a59a785f35e881810af30cc94fbc4ee5
Renan Lavarec <rlavarec@gopro.com>
2023-06-22 10:23:37 +0200

    visual studio: make the script more flexible by supporting internal dev env.

--- scripts/setup_dev_sln.py
+++ scripts/setup_dev_sln.py
@@ -5,12 +5,26 @@ import re
 import xml.etree.ElementTree as ET
 
 # go to root
-os.chdir(os.path.join(os.path.dirname(__file__), ".."))
+repoDir = os.path.join(os.path.dirname(__file__), "..")
+os.chdir(repoDir)
 
 ET.register_namespace("", "http://schemas.microsoft.com/developer/msbuild/2003")
 
 buildir = os.path.join("builddir", "libnodegl")
 
+
+# deploy inside the nodegl dir as isolated
+deployDir = R"$(ProjectDir)\..\..\venv\Scripts"
+
+# check inside the bundle buildir and deply inside the scripts dir
+if os.path.exists(os.path.join(repoDir, "stupeflix", "sripts")):
+    deployDir = R"$(ProjectDir)\..\..\..\..\stupeflix\Scripts"
+
+# check inside the bundle buildir and deply inside the scripts dir
+if os.path.exists(os.path.join(repoDir, "..", ".sx", "stupeflix", "Scripts")):
+    deployDir = R"$(ProjectDir)\..\..\..\.sx\stupeflix\Scripts"
+
+deployDir += "\\"
 
 def getFilename( filename ):
     return os.path.join(buildir, filename)
@@ -36,26 +50,27 @@ with open(getFilename(libnodegl_dev_sln), 'r') as f:
 
     nodegl_vcxproj_data_file = nodegl_vcxproj_data_file.replace(nodegl_vcxproj, nodegl_dev_vcxproj)
 
-    AllProjectEntry = ''
-    AllProjectConfig = ''
-    for name, project in listProjectToInclude.items():
-        with open(getFilename(project), 'r') as f:
-            project_data_file = "\n".join(f.readlines())
-            project_uid = re.search("ProjectGuid>{(.*?)}", project_data_file).group(1)
+    for builtType in ["release", "debug"]:
+        AllProjectEntry = ''
+        AllProjectConfig = ''
+        for name, project in listProjectToInclude.items():
+            with open(getFilename(project), 'r') as f:
+                project_data_file = "\n".join(f.readlines())
+                project_uid = re.search("ProjectGuid>{(.*?)}", project_data_file).group(1)
 
-            AllProjectEntry += 'Project("{'+projectID+'}") = "'+name+'", "'+project+'", "{'+project_uid+'}"' + "\n" + 'EndProject' + "\n"
-            AllProjectConfig += '{'+project_uid+'}.debug|x64.ActiveCfg = debug|x64' + "\n" + '{'+project_uid+'}.debug|x64.Build.0 = debug|x64' + "\n"
+                AllProjectEntry += 'Project("{'+projectID+'}") = "'+name+'", "'+project+'", "{'+project_uid+'}"' + "\n" + 'EndProject' + "\n"
+                AllProjectConfig += '{'+project_uid+'}.'+builtType+'|x64.ActiveCfg = '+builtType+'|x64' + "\n" + '{'+project_uid+'}.'+builtType+'|x64.Build.0 = '+builtType+'|x64' + "\n"
 
-    nodegl_vcxproj_data_file = nodegl_vcxproj_data_file.replace("EndProject\nGlobal\n", 'EndProject\n' + AllProjectEntry + 'Global' + "\n")
-    nodegl_vcxproj_data_file = nodegl_vcxproj_data_file.replace("ActiveCfg = debug|x64\n	EndGlobalSection\n", 'ActiveCfg = debug|x64\n	' + AllProjectConfig + 'EndGlobalSection' + "\n")
+        nodegl_vcxproj_data_file = nodegl_vcxproj_data_file.replace("EndProject\nGlobal\n", 'EndProject\n' + AllProjectEntry + 'Global' + "\n")
+        nodegl_vcxproj_data_file = nodegl_vcxproj_data_file.replace("ActiveCfg = "+builtType+"|x64\n	EndGlobalSection\n", "ActiveCfg = "+builtType+"|x64\n	" + AllProjectConfig + "EndGlobalSection" + "\n")
 
 with open(getFilename(libnodegl_dev_sln), 'w') as f:
     f.write(nodegl_vcxproj_data_file)
 
 
 def getOrCreate(xmlObj, name):
-    tXmlObj = xmlObj.find(name)
-    if not tXmlObj:
+    tXmlObj = xmlObj.find('{*}' + name)
+    if tXmlObj == None:
         return ET.SubElement(xmlObj, name)
     return tXmlObj
 
@@ -71,13 +86,15 @@ def updateProject(projectName, commands):
 
     tree = getVCXProjXml(projectUserFile)
     ProjectXml = tree.getroot()
-    ItemDefinitionGroupXML = getOrCreate(ProjectXml, 'ItemDefinitionGroup')
-    CommandXml = getOrCreate(getOrCreate(ItemDefinitionGroupXML, "CustomBuildStep"), 'Command')
-    CommandXml.set("Condition", "'$(Configuration)|$(Platform)'=='debug|x64'")
-    CommandXml.text = commands
-    OutputsXml = getOrCreate(getOrCreate(ItemDefinitionGroupXML, "CustomBuildStep"), 'Outputs')
-    OutputsXml.set("Condition", "'$(Configuration)|$(Platform)'=='debug|x64'")
-    OutputsXml.text = R'dummy_file.txt'
+
+    for builtType in ["release", "debug"]:
+        ItemDefinitionGroupXML = getOrCreate(ProjectXml, 'ItemDefinitionGroup')
+        CommandXml = getOrCreate(getOrCreate(ItemDefinitionGroupXML, "CustomBuildStep"), 'Command')
+        CommandXml.set("Condition", "'$(Configuration)|$(Platform)'=='"+builtType+"|x64'")
+        CommandXml.text = commands
+        OutputsXml = getOrCreate(getOrCreate(ItemDefinitionGroupXML, "CustomBuildStep"), 'Outputs')
+        OutputsXml.set("Condition", "'$(Configuration)|$(Platform)'=='"+builtType+"|x64'")
+        OutputsXml.text = R'dummy_file.txt'
 
 
     ET.indent(tree, space="\t", level=0)
@@ -89,30 +106,38 @@ def getVCXProjUserXml(filename):
     root = ET.Element('Project')
     root.set("xmlns", "http://schemas.microsoft.com/developer/msbuild/2003")
     root.set("ToolsVersion", "Current")
-    PropertyGroupXml = ET.SubElement(root, "PropertyGroup")
-    PropertyGroupXml.set("Condition", "'$(Configuration)|$(Platform)'=='debug|x64'")
+
+    for builtType in ["release", "debug"]:
+        PropertyGroupXml = ET.SubElement(root, "PropertyGroup")
+        PropertyGroupXml.set("Condition", "'$(Configuration)|$(Platform)'=='"+builtType+"|x64'")
     return ET.ElementTree(root)
 
 
 def updateProjectUser(projectName, command):
     projectUserFile = getFilename(listProjectToInclude[projectName] + ".user")
+
     tree = getVCXProjUserXml(projectUserFile)
     ProjectXml = tree.getroot()
-    for PropertyGroupXML in ProjectXml.findall('PropertyGroup'):
+
+    for PropertyGroupXML in ProjectXml.findall('{*}PropertyGroup'):
         getOrCreate(PropertyGroupXML, "LocalDebuggerCommandArguments").text = command
-        getOrCreate(PropertyGroupXML, "LocalDebuggerCommand").text = R"$(ProjectDir)\..\..\venv\Scripts\$(TargetFileName)"
-        getOrCreate(PropertyGroupXML, "LocalDebuggerWorkingDirectory").text = R"$(ProjectDir)\..\..\venv\Scripts"
+        getOrCreate(PropertyGroupXML, "LocalDebuggerCommand").text = os.path.join(deployDir, "$(TargetFileName)")
+        getOrCreate(PropertyGroupXML, "LocalDebuggerWorkingDirectory").text = deployDir
         getOrCreate(PropertyGroupXML, "DebuggerFlavor").text = R"WindowsLocalDebugger"
 
     ET.indent(tree, space="\t", level=0)
     tree.write(projectUserFile)
 
 
-updateProject("nodegl", R'copy /Y "$(TargetPath)" "$(ProjectDir)\..\..\venv\Scripts\"' + "\n" + R'copy /Y "$(TargetPath)" "$(ProjectDir)\..\..\pynodegl\"')
-updateProject("ngl-python", R'copy /Y "$(TargetPath)" "$(ProjectDir)\..\..\venv\Scripts\"' + "\n" + R'rmdir /S /Q "$(temp)\ngl-desktop\localhost-1234"')
-updateProject("ngl-desktop", R'copy /Y "$(TargetPath)" "$(ProjectDir)\..\..\venv\Scripts\"' + "\n" + R'rmdir /S /Q "$(temp)\ngl-desktop\localhost-1234"')
-updateProject("ngl-serialize", R'copy /Y "$(TargetPath)" "$(ProjectDir)\..\..\venv\Scripts\"' + "\n" + R'rmdir /S /Q "$(temp)\ngl-desktop\localhost-1234"')
+updateProject("nodegl", f'copy /Y "$(TargetPath)" "{deployDir}"' + "\n" + R'copy /Y "$(TargetPath)" "$(ProjectDir)\..\..\pynodegl\"')
+updateProject("ngl-python", f'copy /Y "$(TargetPath)" "{deployDir}"' + "\n" + R'rmdir /S /Q "$(temp)\ngl-desktop\localhost-1234"')
+updateProject("ngl-desktop", f'copy /Y "$(TargetPath)" "{deployDir}"' + "\n" + R'rmdir /S /Q "$(temp)\ngl-desktop\localhost-1234"')
+updateProject("ngl-serialize", f'copy /Y "$(TargetPath)" "{deployDir}"' + "\n" + R'rmdir /S /Q "$(temp)\ngl-desktop\localhost-1234"')
 
+listProjectToInclude = listProjectToInclude | listProjectNodeGLLib
+updateProjectUser("nodegl", "")
 updateProjectUser("ngl-python", R"-b d3d12 $(ProjectDir)\..\..\builddir\tests\..\..\tests\compute.py compute_cubemap_load_store")
 updateProjectUser("ngl-desktop", R"-b d3d12")
-updateProjectUser("ngl-serialize", R"$(ProjectDir)\..\..\builddir\tests\..\..\tests\compute.py compute_cubemap_load_store c:/tmp/compute_cubemap_load_store.ngl")
+updateProjectUser("ngl-serialize", R"$(ProjectDir)\..\..\builddir\tests\..\..\tests\compute.py compute_cubemap_load_store c:\tmp\compute_cubemap_load_store.ngl")
+
+print("setup dev sln -> done")
